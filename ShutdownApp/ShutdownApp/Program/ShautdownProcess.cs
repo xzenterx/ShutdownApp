8cb6a4a465320c516988af9065d16143f5cb645c
Ivan Protsenko <xzenterx7755@gmail.com>
2019-10-17 23:21:30 +0300

    Add function set shutdown

---
+++ ShutdownApp/ShutdownApp/Program/ShautdownProcess.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace ShutdownApp.Program
+{
+    public class ShautdownProcess
+    {
+        private int _seconds;
+
+        private void SetTime(int minutes)
+        {
+            int seconds = minutes * 60;
+            _seconds = seconds;
+        }
+
+        public void SetSutdown(int minutes)
+        {
+            SetTime(minutes);
+            ProcessStartInfo processShutdown = new ProcessStartInfo("cmd", $"/c shutdown -s -f -t {_seconds}" );
+            Process.Start(processShutdown);
+        }
+    }
+}
