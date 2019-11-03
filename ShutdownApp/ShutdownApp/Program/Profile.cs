27340c0cfd6cafb0eccb0b4518d793dbea6c66f6
Ivan Protsenko <xzenterx7755@gmail.com>
2019-11-03 23:20:31 +0200

    Added class profile

---
+++ ShutdownApp/ShutdownApp/Program/Profile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShutdownApp.Program
+{
+    public class Profile
+    {   
+        public string Name { get; set; }
+        public TimeSpan Time { get; set; }
+
+    }
+}
